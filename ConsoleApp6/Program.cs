1b3af5094a787eeda95ca7e9fbeb0260b644f032
perfectoMIA <maremyanov05@mail.ru>
2023-04-02 17:32:05 +0900

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,29 @@
+﻿class Person
+{
+    public int age;
+    public Person (int age)
+    {
+        this.age = age;
+    }
+    public void Print()
+    {
+        Console.WriteLine(age);
+    }
+}
+class Child : Person
+{
+    public Child (int age) : base (age)
+    {
+
+    }
+
+}
+
+
+public class Program
+{
+    static void Main ()
+    {
+
+    }
+}
